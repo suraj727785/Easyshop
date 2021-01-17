ddbeea41500513131e61cb5e8629d2c5f097efb5
surajkr727785 <surajkr727785@gmail.com>
2021-01-17 19:38:47 +0530

    displays shop from database

--- index.php
+++ index.php
@@ -11,13 +11,31 @@ if(isset($_SESSION['username'])){
        <!-- Showcase -->
        
         <!-- Home cards 1 -->
+        
         <section class="home-cards">
+        <?php
+        $query="SELECT * FROM shops";
+        $results=mysqli_query($connection,$query);
+         
+        while($row=mysqli_fetch_assoc($results)){
+          $shop_name=$row['shop_name'];
+          $shop_image=$row['shop_image'];
+          $shop_address=$row['shop_address']
+          ?>
+          
           <div><a href="shop.php">
             <img src="images/shop.jpg" alt=""></a>
-            <h3>Annat Gift Shop</h3>
-            <p>Bypass Road,Gaya,Bihar
+            <h3><?php echo $shop_name ?></h3>
+            <p><?php echo $shop_address ?>
             </p>
             <a href="shop.php">Shop Now <i class="fas fa-chevron-right"></i></a>
           </div>
+       
+        <?php
+        }
+
+        ?> 
+        
         </section>
+        
         <?php  include "includes/footer.php"; ?> 
